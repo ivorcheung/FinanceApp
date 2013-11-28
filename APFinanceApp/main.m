1864b452e8872360d3a09705d1c88259a60b22c5
Ivor Cheung <ivorcheung@Ivor-Cheungs-iMac.local>
2013-11-28 19:29:23 +0000

    Initial Commit

---
+++ APFinanceApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  APFinanceApp
+//
+//  Created by Ivor Cheung on 28/11/2013.
+//  Copyright (c) 2013 Ivor Cheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
